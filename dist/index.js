ae2d7ad1409a3569f812696a94d4a4ba805be38a
johnSalaza <128521323+johnSalaza@users.noreply.github.com>
2023-03-21 14:22:25 -0500

    create project
    ejercicio tsc con cadena de texto y tipado estatico

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+var usuario = prompt("dime tu nombre: ");
+let mensaje = "hola mundo";
+console.log(mensaje + "\n" + usuario);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+var usuario = prompt("dime tu nombre: ");
+let mensaje: string = "hola mundo";
+console.log(mensaje + "\n" + usuario);
